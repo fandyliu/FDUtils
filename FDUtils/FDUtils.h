d75ec680d82dd0009f5e8958690eb250d415e42b
liuhuan <huan.liu@frontpay.cn>
2016-08-22 11:20:00 +0800

    基本配置

---
+++ FDUtils/FDUtils.h
@@ -0,0 +1,19 @@
+//
+//  FDUtils.h
+//  FDUtils
+//
+//  Created by QianTuFD on 16/8/22.
+//  Copyright © 2016年 fandy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FDUtils.
+FOUNDATION_EXPORT double FDUtilsVersionNumber;
+
+//! Project version string for FDUtils.
+FOUNDATION_EXPORT const unsigned char FDUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FDUtils/PublicHeader.h>
+
+
